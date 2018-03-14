5de3af1bf53d42b67e03e0c5119752cd3833e843
root <hapro955@gmail.com>
2018-03-14 13:46:21 +0700

    danh sach sinh vien

--- list.php
+++ list.php
@@ -1,3 +1,40 @@
 <?php
-	echo "list 1";
+	$conn = mysqli_connect("thuctapphp.com", "root", "123456", "thuctapPHP");
+	    if(!$conn)
+	    {
+	    	echo "Connect Failed!". mysqli_connect_error($conn);
+	    }
+	    	echo "Successsful";
+
 ?>
+<html>
+<head></head>
+<body>
+	<table border="1px">
+		<tr>
+			<th>ID</th>
+			<th>Name</th>
+			<th>Status</th>
+			<th>Action</th>
+		</tr>
+		<?php
+				$sql="SELECT * FROM student";
+				$result=mysqli_query($conn,$sql);
+				if(mysqli_num_rows($result) > 0){
+				while($row=mysqli_fetch_assoc($result)){
+					echo '<br/>';	
+		?>
+		<tr>
+			<td><?php echo $row['studentId']; ?></td>
+			<td><?php echo $row['studentName']; ?></td>
+			<td><?php echo $row['studentStatus']; ?></td>
+			<td><a href="edit.php?id=<?php echo $row['studentId']; ?>">Edit |</a><a href="delete.php?id=<?php echo $row['studentId']; ?>">Delete</a></td>		
+		</tr>
+		<?php
+				}
+		}	
+
+		?>
+	</table>
+</body>
+</html>
