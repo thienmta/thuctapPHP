b367c0619d7ed6afe01ae706d165b1e87b408dc5
MinhHai <haipm@deha-soft.com>
2018-03-14 16:21:11 +0700

    up

--- delete.php
+++ delete.php
@@ -1,3 +1,18 @@
 <?php
-	echo "Xoa";
+	function deleteStudent($id){
+		$sql = mysqli_query("UPDATE  student  SET studentStatus='0' WHERE studentId='$id'");
+		return $sql;
+	}
 ?>
+<script language="javascript">
+    	function load_ajax(){
+    		var agrs = {
+    			url : "delete.php";
+    			type  :  "post";
+    			dataType : "text";
+    			data : {
+    				id : $id 
+    			}
+    		}
+    	}
+</script>
